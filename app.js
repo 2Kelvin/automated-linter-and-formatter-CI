9ccb3efa1592ee819b82ba9736ad233a68b47384
2Kelvin <rocketmancodes02@gmail.com>
2026-04-02 17:30:19 +0300

    fix js,css and html formatting

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Simple interaction logic
-const button = document.getElementById('colorBtn');
+const button = document.getElementById("color-btn");
 
 button.addEventListener('click', () => {
-    // Generate a random light color
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = "#" + randomColor;
+  // Generate a random light color
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
 });
