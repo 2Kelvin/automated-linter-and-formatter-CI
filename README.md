9efda5102353b842b2daa68fe18708962936ca34
2Kelvin <rocketmancodes02@gmail.com>
2026-04-02 16:07:21 +0300

    add workflow and complete test code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# automated-linter-and-formatter-CI
-Automated linting and formatting code on pull request using Github Actions
+# Automated Linter and Formatter CI
+Automatically check code style (Linting) and formatting on every Pull Request before merging to main branch using Github Actions
---
+++ app.js
@@ -0,0 +1,8 @@
+// Simple interaction logic
+const button = document.getElementById('colorBtn');
+
+button.addEventListener('click', () => {
+    // Generate a random light color
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+});
