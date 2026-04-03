11266290c83c106d923e0793bf400b395036cd0f
2Kelvin <rocketmancodes02@gmail.com>
2026-04-03 07:08:12 +0300

    use prettier instead of biome

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const button = document.getElementById("color-btn");
 
 button.addEventListener("click", () => {
-	// Generate a random light color
-	const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-	document.body.style.backgroundColor = `#${randomColor}`;
+  // Generate a random light color
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = `#${randomColor}`;
 });
